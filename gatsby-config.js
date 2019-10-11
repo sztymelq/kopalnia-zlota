6ac5d998d9655cf8bd40f334f00b4e78cac01682
Sztymelski, Piotr <sztymelskip@ryanair.com>
2019-10-11 16:51:57 +0200

    Added gatsby cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify-cms`
     // `gatsby-plugin-offline`,
   ],
 };
